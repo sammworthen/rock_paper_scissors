bd603bb96f8b86be94a695fe0dfd52231425db6f
Michael Demerjian <michaeldemerjian@gmail.com>
2017-04-27 19:22:26 -0600

    some changes to the js

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready( function() {
+  //grab the player 1's selection
+  var playerChoice1 = null;
+  var playerChoice2 = Math.random()
+  var $rock = $('#rock')
+  var $paper = $('#paper')
+  var $scissors = $('#scissors')
+
+    if(playerChoice2 < .33) {
+      playerChoice2 == 'rock'
+    } else if (playerChoice2 < .6666) {
+      playerChoice2 =='paper'
+    } else {
+      playerChoice2 == 'scissors'
+    };
+    console.log(playerChoice2)
+
+    
+})
